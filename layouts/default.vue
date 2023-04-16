4fb85d7de19ea29ae12de01a7d0e1c504bf1773e
Nick Dodsley <nick@ndestates.com>
2023-04-16 16:16:46 +0100

    Added nuxt link nav

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
-        <nav>Nav goes here </nav>
+        <nav>
+            <nuxt-link to="/">Home</nuxt-link> |
+            <nuxt-link to="/about">About</nuxt-link> |
+            <nuxt-link to="/contact">Contact</nuxt-link>                  
+        </nav>
         <slot />
     </div>
-</template>
+</template> 
